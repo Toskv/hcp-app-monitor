814dc63d8a78508fea721df740da0ed33c15d72b
Ilie Ciotir <dipixelhunter@gmail.com>
2016-11-13 19:16:00 +0200

    make a get request for the status to hcp

--- src/routes/status.ts
+++ src/routes/status.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { BaseRoute } from "./route";
-import { Agent } from 'http';
+import * as http from 'http';
 
 export class StatusRoute extends BaseRoute {
     public static create(router: Router) {
@@ -17,6 +17,27 @@ export class StatusRoute extends BaseRoute {
     public get(req: Request, res: Response, next: NextFunction) {
         console.log(req.params.account);
         console.log(req.params.app);
-        
+
+        http.get({
+            host: 'api.hana.ondemand.com',
+            path: `/monitoring/v1/accounts/${req.params.account}/apps/${req.params.app}/state`,
+
+        }, (monitorResponse) => {
+            let body = '';
+
+            monitorResponse.on('data', (d) => {
+                body += d;
+            });
+
+            monitorResponse.on('end', () => {
+                console.log(body);
+                res.json("abc");
+            });
+
+            monitorResponse.on('error', (e) => {
+                console.log(e);
+                res.json('error');
+            });
+        });
     }
 }
